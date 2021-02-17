87e394f661da9699e259c19135356ecd644419e9
JoristerWolbeek <joris.terwolbeek@outlook.com>
2021-02-17 12:25:18 +0100

    dwafawfdwada

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Joris ter Wolbeek</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+</head>
+
+
+<header>
+</header>
+
+<body style="margin-top:5%">
+  <div class="container">
+    <div class="row">
+      <div class="one-half column u-max-full-width" >
+        <img src="images/JigenDaisukeCagliostro.png" style="max-width:200px;align-self: center;">
+        <h1 class="textBlue" style="margin:0px;padding:0px;">Joris ter Wolbeek</h1>
+        <p  style="margin:0px;padding:0px;">
+          <a href="https://github.com/JoristerWolbeek"><i class="fab fa-github-square" style="color:#222;" ></i></a>
+          &nbsp;
+          &nbsp;
+          <a href="https://www.linkedin.com/in/joris-ter-wolbeek/" target="_blank"><i class="fab fa-linkedin-in" style="color:#222;"></i></a>
+          &nbsp;
+          &nbsp;
+          <a href="" target=""><i class="fas fa-paperclip" style="color:#222;"></i></a>
+        </p>
+        <h5>BIT student</h5>
+      </div>
+    
+    <div class="row">
+      <div class="one-half column u-max-full-width" >
+        <h2 style="margin:0px;padding:0px;" class="textBlue">Work experience</h2>
+        <p style="margin:0px;padding:5px;">Internship @ <a href="https://www.brainblurb.com/" target="_blank">Brainblurb</a></p>
+        <p style="margin:0px;padding:5px;">Partime @ Japanese delicatesse Atariya</a></p>
+        <p style="margin:0px;padding:5px;">Partime @ HMSHost</a></p>
+        <p style="margin:0px;padding:5px;">Partime @ Bakker Bertram</a></p>
+        <p></p>
+        <h4 class="textBlue" style="margin:0px;padding:0px;">Skills</h4>
+          <h6 style="margin:0px;padding:0px;">Frontend</h6>
+          <span class="tag">HTML</span>
+          <span class="tag">CSS</span>
+          <h6 style="margin-bottom:0px;margin-top:0px;padding:0px;">Backend</h6>
+          <span class="tag">JS</span>
+          <span class="tag">PHP</span>
+          <span class="tag">MSQL</span>
+      </div>
+    </div>
+    <p></p>
+    <div class="row">
+      <div class="u-max-full-width">
+        <h2 class="textBlue" style="margin:0px;padding:0px;">About me</h2>
+        <p>A student who is eager to learn and happy to be studying at Bit Academy. Plays Magic The Gathering in his free time.</p>
+      </div>
+  </div>
+  <div class="row">
+    <div class="twelve columns u-max-full-width" >
+      <h2 class="textBlue">Projects</h2>
+      <p>To come.</p>
+    </div>
+  <div class="row">
+    <div class="twelve columns u-max-full-width" >
+      <p><i>
+        Greatness at any cost.
+      </i></p>
+    </div>
+</div>
+  
+</div>
+</body>
+
+</html>
